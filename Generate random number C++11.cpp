873d69e32972cdfbf3360e18fd74b10e9e54a8f7
Chikh Volodymyr <chikhvol@gmail.com>
2015-07-04 10:27:16 +0300

    Add random number generation

---
+++ Generate random number C++11.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <cstring>
+#include <vector>
+#include <list>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <numeric>
+#include <ctime>
+#include <fstream>
+#include <iomanip>
+#include <stdexcept>
+#include <functional>
+#include <math.h>
+#include <utility>
+#include <random>
+
+#pragma comment(linker, "/STACK:133217728")
+
+using namespace std;
+
+int main() {  
+	//freopen("1.docx", "r", stdin);
+	//freopen("out.txt", "w", stdout);
+	ios_base::sync_with_stdio(0);
+	random_device rd;
+    mt19937 mt(rd());
+	uniform_int_distribution<int> dist(1, 10000);
+
+    for (int i=0; i<20; ++i)
+        std::cout << dist(mt) << "\n";
+	return 0;
+}
+
